c6c9dbe41049b971282356aad28d4e236bd670ce
JUN JIAN SHEN <jshen9085@gmail.com>
2018-06-22 04:09:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function dwarfRollCall(dwarves) {
+  var string = '';
+  for (var i = 0; i < dwarves.length; i++){
+    var num = i + 1;
+    var element = '. ' + dwarves[i] + ' ';
+    string += num + element;
+  }
+  return string; 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = [];
+  for (var i = 0; i < planeteerCalls.length; i++){
+    var element = planeteerCalls[i] + '!';
+    arr.push(element);
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++){
+    var word = words[i];
+    if (word.length > 4){
+      return true
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ['cheddar', 'gouda', 'camembert'];
+  for (var i = 0; i < foods.length; i++){
+    if (cheese.indexOf(foods[i]) > -1){
+      return foods[i];
+    } 
+  }
+  return "no cheese!"
 }
+
+
+
+
